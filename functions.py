1067700430cda0ca273cccc8ca4d8e61c7bf6e01
joan-kii <joan.sb@hotmail.com>
2022-07-22 06:01:14 +0200

    añade logo

--- functions.py
+++ functions.py
@@ -11,7 +11,7 @@ widgets: dict[str, list[str]] = {
 
 # Helpers
 
-def clear_widgets():
+def clear_widgets() -> None:
     """
     Clear all the widget in the view
     :return: None
@@ -21,6 +21,28 @@ def clear_widgets():
             widgets[widget][-1].hide()
         for _ in range(0, len(widgets[widget])):
             widgets[widget].pop()
+
+def create_buttons(txt: str, l_margin: int, r_margin: int) -> None:
+    button = QPushButton(txt)
+    button.setCursor(QCursor(QtCore.Qt.CursorShape.PointingHandCursor))
+    button.setFixedWidth(485)
+    button.setStyleSheet(
+        """
+        border: 4px solid '#ff595e';
+        color: white;
+        font-family: 'shanti';
+        font-size: 16px;
+        border-radius: 25px;
+        padding: 15px 0;
+        margin-top: 20px;
+        }
+        *:hover{
+            background: '#ff595e';
+        }
+        """
+    )
+
+    button.clicked
             
 
 # Frames
@@ -28,3 +50,10 @@ def clear_widgets():
 def frame_1():
     clear_widgets()
     image = QPixmap(".assets/logo.png")
+    logo = QLabel()
+    logo.setPixmap(image)
+    logo.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+    logo.setStyleSheet("margin-top: 100px;")
+    widgets["logo"].append(logo)
+
+
